6b3807eb5fb8d313c65ad63ee4fcb60eac226306
Keznikl, Matej <matej.keznikl@thermofisher.com>
2025-10-22 15:08:56 +0200

    ADD: SkeletonizationWebApplication setup

---
+++ SkeletonizationWebApplication/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Skeletonization Web Application"
+};
+
+const RootLayout = ({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) => (
+  <html lang="en">
+    <body>{children}</body>
+  </html>
+);
+
+export default RootLayout;
