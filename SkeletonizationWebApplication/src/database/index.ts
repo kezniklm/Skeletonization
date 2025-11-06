8f028bdb6db6e401a58a2040edecc8fc55e9124c
Keznikl, Matej <matej.keznikl@thermofisher.com>
2025-11-06 08:38:25 +0100

    ADD: setup database

---
+++ SkeletonizationWebApplication/src/database/index.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import "dotenv/config";
+
+import { drizzle } from "drizzle-orm/libsql";
+import postgres from "postgres";
+
+import { account, session, user, verification } from "./schema/auth";
+
+const client = postgres(process.env.DATABASE_URL as string);
+
+export const db = drizzle(client, {
+  schema: {
+    user,
+    session,
+    account,
+    verification
+  }
+});
+
+export type DB = typeof db;
