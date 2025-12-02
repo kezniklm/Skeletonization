b8123cc8e94fd3ae134f11791d6b22741c6a1d4a
Keznikl, Matej <matej.keznikl@thermofisher.com>
2025-12-02 14:48:49 +0100

    FIX: layout on laptop

--- SkeletonizationWebApplication/src/components/layout/private-layout.tsx
+++ SkeletonizationWebApplication/src/components/layout/private-layout.tsx
@@ -14,7 +14,7 @@ export const PrivateLayout = ({
   return (
     <div className="flex min-h-screen flex-col">
       <Navigation navigationBadges={privateNavigationBadges} navigationItems={navigationItems} />
-      <main className="container mx-auto px-8 py-16 lg:max-w-4/5">{children}</main>
+      <main className="container mx-auto px-8 py-13 lg:max-w-4/5 2xl:py-16">{children}</main>
     </div>
   );
 };
